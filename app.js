78ae80e272a2c0d6b194d17c62397282e0334f05
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2025-04-09 18:02:49 +0300

    Реализовал асинххронное чтение из текстового файла

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 
+// реализовал чтение файла в асинхронном варианте(Для наглядности сделал его с другим именем
+// и раньше(чтобы вызывался первым, а обрабатывался вторым))
+fs.readFile('helloAsync.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Ошибка чтения файла', err);
+        return;
+    }
+    console.log(data);
+});
+
 //реализовал чтение файла в синхронном варианте
 let fileContent = fs.readFileSync('hellosync.txt', 'utf8')
 console.log(fileContent);
