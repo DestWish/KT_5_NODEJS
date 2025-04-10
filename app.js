eb78ac79db9fa62c274f5240b4eb46ed97cb5138
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2025-04-10 13:10:46 +0300

    Реализовал 'Дозапись' текстового файла в асинхронном варианте, изменил строение кода для правильности выполнения и отображения в терминале

--- app.js
+++ app.js
@@ -10,65 +10,61 @@ fs.readFile('helloAsync.txt', 'utf8', (err, data) => {
     console.log(data);
 });
 
-//реализовал чтение файла в синхронном варианте
-let fileContent = fs.readFileSync('hellosync.txt', 'utf8')
-console.log(fileContent);
 
 // реализовал запись текстового файла в асинхронном варианте
 fs.writeFile('loveAsync.txt', 'I LOVE ITHUB <3 Async', (err) => {
     if (err) {
-        console.error('Ошибка записи файла', err);
+        console.error('Ошибка записи файла 1', err);
         return;
     }
-    console.log('Файл loveAsync.txt записан');
+    console.log('Файл loveAsync.txt записан 1');
 
 
     // читаем файл после записи
     fs.readFile('loveAsync.txt', 'utf8', (err, data) => {
         if (err) {
-            console.error('Ошибка чтения файла', err);
+            console.error('Ошибка чтения файла 2', err);
             return;
         }
-        console.log(`Файл loveAsync.txt записан: ${data}`);
+        console.log(`Файл loveAsync.txt прочитан 2: ${data}`);
+
+
+        // реализовал дозапись текстового файла в асинхронном варианте
+        fs.appendFile('loveAsync.txt', ' I LOVE NODE.JS <3 Async', (err) => {
+            if (err) {
+                console.error('Ошибка записи файла 3', err);
+                return;
+            }
+            console.log('Файл loveAsync.txt "дозаписан" 3');
+
+
+            // читаем файл после дозаписи
+            fs.readFile('loveAsync.txt', 'utf8', (err, data) => {
+                if (err) {
+                    console.error('Ошибка чтения файла 4', err);
+                    return;
+                }
+                console.log(`Файл loveAsync.txt прочитан после дозаписи 4: ${data}`);
+
+
+
+            });
+        });
     });
-
-    // реализовал дозапись текстового файла в асинхронном варианте
-    fs.appendFile('loveAsync.txt', ' I LOVE NODE.JS <3 Async', (err) => {
-        if (err) {
-            console.error('Ошибка записи файла', err);
-            return;
-        }
-        console.log('Файл loveAsync.txt "дозаписан"');
-    });
-
-    // читаем файл после дозаписи
-    fs.readFile('loveAsync.txt', 'utf8', (err, data) => {
-        if (err) {
-            console.error('Ошибка чтения файла', err);
-            return;
-        }
-        console.log(`Файл loveAsync.txt записан: ${data}`);
-    })
-
-
-    // Реализовал удаление текстового файла в асинхронном варианте(после чтения)
-    fs.unlink('loveAsync.txt', (err) => {
-        if (err) {
-            console.error('Ошибка удаления файла', err);
-            return;
-        }
-        console.log('Файл loveAsync.txt удален');
-    });    
 });
 
+
+//реализовал чтение файла в синхронном варианте
+let fileContent = fs.readFileSync('hellosync.txt', 'utf8')
+console.log(`Синхронное чтение файла ${fileContent}`);
 // реализовал запись текстового файла в синхронном варианте
 fs.writeFileSync('loveSync.txt', 'I LOVE ITHUB <3 Sync');
 fileContent = fs.readFileSync('loveSync.txt', 'utf8')
-console.log(`Файл loveSync.txt записан: ${fileContent}`); 
+console.log(`Синхронно записан файл loveSync.txt: ${fileContent}`);
 // реализовал дозапись текстового файла в синхронном варианте
 fs.appendFileSync('loveSync.txt', ' I LOVE NODE.JS <3 Sync');
 fileContent = fs.readFileSync('loveSync.txt', 'utf8')
-console.log(`Файл loveSync.txt "дозаписан": ${fileContent}`);
+console.log(`Синхронно "дозаписан" файл loveSync.txt: ${fileContent}`);
 // Реализовал удаление текстового файла в синхронном варианте(после чтения)
 fs.unlinkSync('loveSync.txt');
-console.log('Файл loveSync.txt удален');
+console.log('Синхронно файл loveSync.txt');
