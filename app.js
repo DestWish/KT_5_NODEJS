835477275d261c070dcb401af5e6ed66498c19f4
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2025-04-11 11:59:24 +0300

    Реализовал удаление текстового файла в синхронном варианте

--- app.js
+++ app.js
@@ -8,52 +8,52 @@ fs.readFile('helloAsync.txt', 'utf8', (err, data) => {
         return;
     }
     console.log(data);
-});
 
 
-// реализовал запись текстового файла в асинхронном варианте
-fs.writeFile('loveAsync.txt', 'I LOVE ITHUB <3 Async', (err) => {
-    if (err) {
-        console.error('Ошибка записи файла 1', err);
-        return;
-    }
-    console.log('Файл loveAsync.txt записан 1');
-
-
-    // читаем файл после записи
-    fs.readFile('loveAsync.txt', 'utf8', (err, data) => {
+    // реализовал запись текстового файла в асинхронном варианте
+    fs.writeFile('loveAsync.txt', 'I LOVE ITHUB <3 Async', (err) => {
         if (err) {
-            console.error('Ошибка чтения файла 2', err);
+            console.error('Ошибка записи файла 1', err);
             return;
         }
-        console.log(`Файл loveAsync.txt прочитан 2: ${data}`);
+        console.log('Файл loveAsync.txt записан 1');
 
 
-        // реализовал дозапись текстового файла в асинхронном варианте
-        fs.appendFile('loveAsync.txt', ' I LOVE NODE.JS <3 Async', (err) => {
+        // читаем файл после записи
+        fs.readFile('loveAsync.txt', 'utf8', (err, data) => {
             if (err) {
-                console.error('Ошибка записи файла 3', err);
+                console.error('Ошибка чтения файла 2', err);
                 return;
             }
-            console.log('Файл loveAsync.txt "дозаписан" 3');
+            console.log(`Файл loveAsync.txt прочитан 2: ${data}`);
 
 
-            // читаем файл после дозаписи
-            fs.readFile('loveAsync.txt', 'utf8', (err, data) => {
+            // реализовал дозапись текстового файла в асинхронном варианте
+            fs.appendFile('loveAsync.txt', ' I LOVE NODE.JS <3 Async', (err) => {
                 if (err) {
-                    console.error('Ошибка чтения файла 4', err);
+                    console.error('Ошибка записи файла 3', err);
                     return;
                 }
-                console.log(`Файл loveAsync.txt прочитан после дозаписи 4: ${data}`);
+                console.log('Файл loveAsync.txt "дозаписан" 3');
 
 
-                // Реализовал удаление текстового файла в асинхронном варианте(после чтения)
-                fs.unlink('loveAsync.txt', (err) => {
+                // читаем файл после дозаписи
+                fs.readFile('loveAsync.txt', 'utf8', (err, data) => {
                     if (err) {
-                        console.error('Ошибка удаления файла 5', err);
+                        console.error('Ошибка чтения файла 4', err);
                         return;
                     }
-                    console.log('Файл loveAsync.txt удален 5');
+                    console.log(`Файл loveAsync.txt прочитан после дозаписи 4: ${data}`);
+
+
+                    // Реализовал удаление текстового файла в асинхронном варианте(после чтения)
+                    fs.unlink('loveAsync.txt', (err) => {
+                        if (err) {
+                            console.error('Ошибка удаления файла 5', err);
+                            return;
+                        }
+                        console.log('Файл loveAsync.txt удален 5');
+                    });
                 });
             });
         });
@@ -61,9 +61,12 @@ fs.writeFile('loveAsync.txt', 'I LOVE ITHUB <3 Async', (err) => {
 });
 
 
+
+
+
 //реализовал чтение файла в синхронном варианте
 let fileContent = fs.readFileSync('hellosync.txt', 'utf8')
-console.log(`Синхронное чтение файла ${fileContent}`);
+console.log(`Синхронное чтение файла: ${fileContent}`);
 // реализовал запись текстового файла в синхронном варианте
 fs.writeFileSync('loveSync.txt', 'I LOVE ITHUB <3 Sync');
 fileContent = fs.readFileSync('loveSync.txt', 'utf8')
@@ -74,4 +77,4 @@ fileContent = fs.readFileSync('loveSync.txt', 'utf8')
 console.log(`Синхронно "дозаписан" файл loveSync.txt: ${fileContent}`);
 // Реализовал удаление текстового файла в синхронном варианте(после чтения)
 fs.unlinkSync('loveSync.txt');
-console.log('Синхронно файл loveSync.txt');
+console.log('Синхронно удален файл loveSync.txt');
